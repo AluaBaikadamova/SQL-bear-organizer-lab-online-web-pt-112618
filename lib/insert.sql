3efd977c3d15ce23f95bd0956ae638b1d91c224f
Alua Baikadamova <abaikad00@gmail.com>
2019-02-09 15:53:42 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (1,"Mr.Chocolate", 3, "male", "white", "angry", true);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (1,"Mr.Chocolate", 3, "male", "white", "angry", true);
