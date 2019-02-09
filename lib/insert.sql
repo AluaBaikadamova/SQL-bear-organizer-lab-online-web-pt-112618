b3511ff782201590ac15d67f5d6e4faea0826333
Alua Baikadamova <abaikad00@gmail.com>
2019-02-09 16:03:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (1,"Mr.Chocolate", 3, "male", "white", "angry", true);
-INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (1,"Mr.Chocolate", 3, "male", "white", "angry", true);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (2,"Rowdy", 3, "male", "white", "angry", true);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (3,"Tabitha", 3, "female", "white", "angry", true);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (4,"Sergeant Brown", 3, "male", "brown", "happy", true);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (5,"Melissa", 3, "female", "white", "angry", true);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (6,"Grinch", 3, "male", "white", "angry", true);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (7,"Wendy", 3, "male", "white", "angry", true);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (8,, 3, "male", "white", "angry", false);
